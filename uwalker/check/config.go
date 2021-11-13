b706de88b0aef1fd0c4cdec1298b106ea4a21ca9
Andrew Berlin <theqjex@gmail.com>
2021-11-13 20:22:29 +0300

    obsolete architecture

--- uwalker/check/config.go
+++
@@ -1,9 +0,0 @@
-package check
-
-type Config struct {
-	CDIRfile    string `json:"cdir-file"`
-	TestHost    string `json:"test-host"`
-	Workers     int    `json:"workers"`
-	HTTPworkers int    `json:"http-workers"`
-	Ports       []int  `json:"ports"`
-}
--- uwalker/check/socks5.go
+++ uwalker/check/socks5.go
@@ -2,7 +2,7 @@ package check
 
 import (
 	"github.com/fangdingjun/socks-go"
-	log "github.com/sirupsen/logrus"
+	"log"
 	"net"
 	"strconv"
 	"strings"
@@ -13,12 +13,12 @@ type Socks5Checker struct {
 }
 
 func (sc *Socks5Checker) Check(checkHost, testHost string) bool {
-	log.Tracef("socks5: Checking %s", checkHost)
+	log.Print("socks5: Checking %s", checkHost)
 	conn, err := net.DialTimeout("tcp", checkHost, 5*time.Second)
 
 	if err != nil {
 		if strings.Contains(err.Error(), "too many open files") {
-			log.Warn(err)
+			log.Print(err)
 		}
 		return false
 	}
@@ -26,19 +26,19 @@ func (sc *Socks5Checker) Check(checkHost, testHost string) bool {
 	s := socks.Client{Conn: conn}
 	err = s.SetDeadline(time.Now().Add(5 * time.Second))
 	if err != nil {
-		log.Trace("Error setting deadline: ", err)
+		log.Print("Error setting deadline: ", err)
 		return false
 	}
 	hostName := strings.TrimRight(testHost, ":")
 	port, _ := strconv.Atoi(strings.TrimLeft(testHost, ":"))
 	err = s.Connect(hostName, uint16(port))
 	if err != nil {
-		log.Trace(err)
+		log.Print(err)
 		return false
 	}
 	err = conn.Close()
 	if err != nil {
-		log.Warn(err)
+		log.Print(err)
 	}
 	return true
 }
