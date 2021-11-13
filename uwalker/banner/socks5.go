6d4e30998a0faaa30b218f70ce7821890fb6218b
Andrew Berlin <theqjex@gmail.com>
2021-11-13 20:20:52 +0300

    socks5 banners

---
+++ uwalker/banner/socks5.go
@@ -0,0 +1,19 @@
+package banner
+
+import (
+	"uwalker/socks5"
+)
+
+type Socks5 struct {
+}
+
+func (s *Socks5) Read(data []byte) ([]byte, int, bool) {
+	if len(data) > 1 && data[0] == socks5.VersionByte && data[1] == 0x00 {
+		return nil, len(data), true
+	}
+	return nil, 0, false
+}
+
+func (s *Socks5) Init() []byte {
+	return socks5.NoAuthHeader
+}
